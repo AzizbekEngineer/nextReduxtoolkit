b8885b6c21823da45ef30bc3901fe3bd6d835803
AzizbekEngineer <azizbekengineer@gmail.com>
2024-07-11 08:52:35 +0500

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { increment } from "@/app/lib/features/counter/counterSlice";
+import { decrement, increment } from "@/app/lib/features/counter/counterSlice";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -9,6 +9,7 @@ const Header = () => {
   return (
     <div>
       <h2>Header {count}</h2>
+      <button onClick={() => dispatch(decrement())}>-</button>
       <button onClick={() => dispatch(increment())}>+</button>
     </div>
   );
